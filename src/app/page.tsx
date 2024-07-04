bb566ded99ae55f8a039829b0af0e16db74cdc86
Rodri Marchese <rodrimarchese@gmail.com>
2024-07-04 18:20:41 -0300

    mvp version, first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,162 @@
-import Image from "next/image";
+// pages/index.js
+"use client";
+import React, { useState, useEffect, useRef } from "react";
+
+const Timer = () => {
+  const [workTime, setWorkTime] = useState(10); // Tiempo de cada bloque de ejercicio en segundos
+  const [modules, setModules] = useState(3); // Número de módulos (grupos de ejercicios)
+  const [exercisesPerModule, setExercisesPerModule] = useState(3); // Ejercicios por módulo
+  const [blocksPerExercise, setBlocksPerExercise] = useState(6); // Bloques por ejercicio
+  const [currentSecond, setCurrentSecond] = useState(0);
+  const [activeTimer, setActiveTimer] = useState(false);
+  const [currentModule, setCurrentModule] = useState(1);
+  const [currentExercise, setCurrentExercise] = useState(1);
+  const [currentBlock, setCurrentBlock] = useState(1);
+
+  const exerciseChangeSound = useRef(new Audio("/Tiempo.mp3"));
+  const moduleChangeSound = useRef(new Audio("/CambioDeCircuito.mp3"));
+  const startSound = useRef(new Audio("/Va.mp3"));
+
+  useEffect(() => {
+    let interval = null;
+
+    if (activeTimer) {
+      interval = setInterval(() => {
+        setCurrentSecond(currentSecond + 1);
+        if (currentSecond >= workTime - 1) {
+          exerciseChangeSound.current.play(); // Sonido al final de cada bloque
+          setCurrentSecond(0);
+          setCurrentBlock(currentBlock + 1);
+          if (currentBlock >= blocksPerExercise) {
+            setCurrentBlock(1);
+            setCurrentExercise(currentExercise + 1);
+            if (currentExercise >= exercisesPerModule) {
+              setCurrentExercise(1);
+              setCurrentModule(currentModule + 1);
+              if (currentModule > modules) {
+                setActiveTimer(false);
+                alert("Entrenamiento completado!");
+                setCurrentModule(1);
+              } else {
+                moduleChangeSound.current.play(); // Sonido de cambio de módulo al cambiar de circuito
+              }
+            }
+          }
+        }
+      }, 1000);
+    } else {
+      clearInterval(interval || 0);
+    }
+
+    return () => clearInterval(interval || 0);
+  }, [
+    activeTimer,
+    currentSecond,
+    workTime,
+    modules,
+    exercisesPerModule,
+    blocksPerExercise,
+    currentModule,
+    currentExercise,
+    currentBlock,
+  ]);
+
+  return (
+    <div className="min-h-screen bg-blue-900 text-white flex flex-col items-center justify-center p-4">
+      <div className="space-y-6">
+        <h1 className="text-3xl font-bold">Cronómetro de Circuito</h1>
+        <div>
+          <label className="block text-white text-lg font-bold mb-2">
+            Tiempo de trabajo (seg):
+          </label>
+          <input
+            type="number"
+            value={workTime}
+            onChange={(e) => setWorkTime(parseInt(e.target.value))}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+        <div>
+          <label className="block text-white text-lg font-bold mb-2">
+            Cantidad de módulos:
+          </label>
+          <input
+            type="number"
+            value={modules}
+            onChange={(e) => setModules(parseInt(e.target.value))}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+        <div>
+          <label className="block text-white text-lg font-bold mb-2">
+            Ejercicios por módulo:
+          </label>
+          <input
+            type="number"
+            value={exercisesPerModule}
+            onChange={(e) => setExercisesPerModule(parseInt(e.target.value))}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+        <div>
+          <label className="block text-white text-lg font-bold mb-2">
+            Bloques por ejercicio:
+          </label>
+          <input
+            type="number"
+            value={blocksPerExercise}
+            onChange={(e) => setBlocksPerExercise(parseInt(e.target.value))}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+        <div className="flex justify-between space-x-4">
+          <button
+            onClick={() => {
+              if (
+                !activeTimer &&
+                currentSecond === 0 &&
+                currentModule === 1 &&
+                currentExercise === 1 &&
+                currentBlock === 1
+              ) {
+                startSound.current.play();
+              }
+              setActiveTimer(!activeTimer);
+            }}
+            className="bg-green-900 hover:bg-green-800 text-white font-bold py-2 px-6 rounded focus:outline-none focus:shadow-outline flex-grow"
+          >
+            {activeTimer ? "Pausar" : "Iniciar"}
+          </button>
+          <button
+            onClick={() => {
+              setActiveTimer(false);
+              setCurrentSecond(0);
+              setCurrentModule(1);
+              setCurrentExercise(1);
+              setCurrentBlock(1);
+            }}
+            className="bg-green-900 hover:bg-green-800 text-white font-bold py-2 px-6 rounded focus:outline-none focus:shadow-outline flex-grow"
+          >
+            Resetear
+          </button>
+        </div>
+        <div className="text-center text-xl">
+          <h2>
+            Circuito/módulo actual: {currentModule}/{modules}, Ejercicio actual:{" "}
+            {currentExercise}/{exercisesPerModule}, Bloque de ej actual:{" "}
+            {currentBlock}/{blocksPerExercise}
+          </h2>
+          <h2>Tiempo: {currentSecond} segundos</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <div>
+      <Timer />
+    </div>
   );
 }
